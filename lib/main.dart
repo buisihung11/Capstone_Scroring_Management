b1cd94b1419b282e2d0982af55d7c32ddcfdd363
buisihung11 <38785848+buisihung11@users.noreply.github.com>
2020-05-22 21:04:24 +0700

    Update title

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
         // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Hello World'),
+      home: MyHomePage(title: 'Capstone Scroing Management'),
     );
   }
 }
